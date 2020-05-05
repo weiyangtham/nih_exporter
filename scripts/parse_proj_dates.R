51fd26ffc41ae33886ce057188c6b6884dd3f499
Wei Yang Tham <weiyang.tham@gmail.com>
2020-05-05 15:53:26 -0400

    split up code to read and clean exporter projects into functions in separate scripts

---
+++ scripts/parse_proj_dates.R
@@ -0,0 +1,56 @@
+# Parse dates, allowing for different formats
+# x = c("1995-01-01T00:00:00", "1/1/1985")
+# df = data_frame(d = x)
+# 
+# df %>% mutate(y = datefun(d))
+
+datefun = function(x){
+  is_datetime = stringr::str_length(x) == 19
+  x_datetime = if_else(is_datetime, x, NA_character_)
+  x_date = if_else(!is_datetime, x, NA_character_)
+  
+  k1 = sum(is_datetime, na.rm = TRUE)
+  k2 = sum(!is_datetime, na.rm = TRUE)
+  
+  nx = sum(!is.na(x), na.rm = TRUE)
+  
+  if (k1 + k2 != nx){stop('formats do not capture all non-missing values')}
+  
+  if (k1 > 0 & k2 > 0){
+    message("mixture of dates and date-times")
+    # mixture of dates and date-times
+    x_parsed = if_else(is_datetime, lubridate::date(lubridate::ymd_hms(x_datetime)), lubridate::mdy(x_date))
+  } else if (k1 == nx & k2 == 0){
+    message("date-times only")
+    # this is the all are date-time case
+    x_parsed = lubridate::date(lubridate::ymd_hms(x_datetime))
+  } else if (k1 == 0 & k2 == nx) {
+    message("dates only")
+    x_parsed = lubridate::mdy(x_date)
+  }
+  x_parsed
+}
+
+
+parse_proj_dates <- function(data, datecols = c("award_notice_date", "budget_start", "budget_end", "project_start", "project_end")){
+  
+  data <- data %>% 
+    mutate_at(vars(one_of(datecols)), list("parsed" = ~ datefun(.))) 
+  
+  k <- data %>% 
+    summarise_at(vars(one_of(c(datecols, paste0(datecols, "_parsed")))), ~sum(is.na(.))) %>% 
+    gather(k, v) %>% 
+    mutate(parsed = if_else(str_detect(k, "parsed"), "parsed", "notparsed"), 
+           k = str_replace(k, "_parsed", "")) %>% 
+    spread(parsed, v) %>% 
+    filter(parsed != notparsed) %>% 
+    nrow()
+  
+  assertthat::assert_that(assertthat::are_equal(k, 0), 
+                          msg = "original date and parsed date have different number of missing values")
+  
+  # Replace original dates with parsed dates
+  data %>% 
+    select(-all_of(datecols)) %>% 
+    rename_at(paste0(datecols, "_parsed"), ~datecols)
+}
---
+++ scripts/read_exporter_proj.R
@@ -0,0 +1,24 @@
+
+# Read in zipped ExPorter project files
+read_exporter_proj <- function(fyr, projfile = "RePORTER_PRJ_C_FY", destdir = "/Volumes/research_data/nihexporter/projects/"){
+  
+  proj_filename <- str_c(projfile, fyr, ".csv")
+  
+  message("extract ", str_c(projfile, fyr, ".csv"), " from \n", str_c(destdir, proj_filename))
+  
+  # Direct/indirect costs appear from 2012 onwards
+  if (fyr < 2012){
+    df <- readr::read_csv(unz(str_c(destdir, projfile, fyr, ".zip"), proj_filename),
+                    col_types = cols(.default = 'c', FY = 'i', 
+                                     TOTAL_COST = 'i', TOTAL_COST_SUB_PROJECT = 'i'),
+                    n_max = Inf)
+  } else {
+    df <- readr::read_csv(unz(str_c(destdir, projfile, fyr, ".zip"), proj_filename),
+                    col_types = cols(.default = 'c', FY = 'i', 
+                                     TOTAL_COST = 'i', TOTAL_COST_SUB_PROJECT = 'i',
+                                     DIRECT_COST_AMT = 'i', INDIRECT_COST_AMT = 'i'),
+                    n_max = Inf)
+  }
+  
+  df %>% janitor::clean_names()
+}
