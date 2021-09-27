c3a08f5ade184f5e12e4a25590b91dfdeb72b2d5
Wei Yang Tham <weiyang.tham@gmail.com>
2021-09-27 11:53:47 -0400

    application type as integer

--- scripts/read_exporter_proj.R
+++ scripts/read_exporter_proj.R
@@ -12,12 +12,12 @@ read_exporter_proj <- function(fyr, projfile = "RePORTER_PRJ_C_FY", destdir = "/
   # Direct/indirect costs appear from 2012 onwards
   if (fyr < 2012){
     df <- readr::read_csv(unz(str_c(destdir, projfile, fyr, ".zip"), proj_filename),
-                    col_types = cols(.default = 'c', FY = 'i', 
+                    col_types = cols(.default = 'c', FY = 'i', APPLICATION_TYPE = 'i',
                                      TOTAL_COST = 'i', TOTAL_COST_SUB_PROJECT = 'i'),
                     n_max = Inf)
   } else {
     df <- readr::read_csv(unz(str_c(destdir, projfile, fyr, ".zip"), proj_filename),
-                    col_types = cols(.default = 'c', FY = 'i', 
+                    col_types = cols(.default = 'c', FY = 'i', APPLICATION_TYPE = 'i',
                                      TOTAL_COST = 'i', TOTAL_COST_SUB_PROJECT = 'i',
                                      DIRECT_COST_AMT = 'i', INDIRECT_COST_AMT = 'i'),
                     n_max = Inf)
