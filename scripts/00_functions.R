3586e3d8e615c4ae9391df0ab158faf41ae06427
Wei Yang Tham <weiyang.tham@gmail.com>
2021-09-28 18:17:58 -0400

    update api. function to extract variables from reporter api results

--- scripts/00_functions.R
+++ scripts/00_functions.R
@@ -7,12 +7,22 @@ ask_reporter <- function(appids){
     appids = list(appids)
   }
   
-  url <- "https://api.reporter.nih.gov/v1/projects/Search"
+  url <- "https://api.reporter.nih.gov/v2/projects/Search"
   json_body <- jsonlite::toJSON(list(criteria = list(appl_ids=appids), limit = "500"), auto_unbox = TRUE)
   res <- POST(url, body=json_body, encode='raw', add_headers('Content-Type'='application/json'))
   
   content(res)[[2]]
   
+}
+
+# Take in a list of results from pinging the RePORTER API
+# and return the desired variable as a tibble
+extract_reporter_variable = function(reporter_list, var){
+  map_df(reporter_list,
+         ~{value = pluck(., var)
+         tibble(application_id = pluck(., "appl_id"),
+                 total_cost = ifelse(is_null(value), NA, value))
+         })
 }
 
 ask_reporter_for_cost = function(appids){
