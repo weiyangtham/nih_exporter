ecff71883161d7596a3abbb1f0d79dd903abc78b
Wei Yang Tham <weiyang.tham@gmail.com>
2021-09-29 13:30:59 -0400

    use original names for updated variables

--- scripts/05-01_replace-missing-cost.R
+++ scripts/05-01_replace-missing-cost.R
@@ -25,25 +25,24 @@ v = seq_last(0, n, by = step)
 # Ping API in batches of size `step` ----
 message("Pinging API takes a while. Uncomment if want to do it")
 
-appids_list = map(head(seq_along(v), -1), 
+appids_list = map(head(seq_along(v), -1),
                   ~{a = v[.] + 1
-                  b = v[. + 1] 
+                  b = v[. + 1]
                   k = seq(a, b, by = 1)
                   appids_subset <- appids[k]
                   ask_reporter(appids_subset)
                   })
 
-assertthat::assert_that(length(appids_list) == (length(v) - 1), 
+assertthat::assert_that(length(appids_list) == (length(v) - 1),
                         msg = "Number of batches of records retrieved is correct")
 appid_cost = map_df(appids_list, ~extract_reporter_variable(., "award_amount"))
-appid_cost %<>% rename(total_cost = name)
+appid_cost %<>% rename(total_cost_api = name)
 
 write_rds(appid_cost, here::here("data/appid-totalcost-reporterapi.rds"))
 
 # Link application IDs back to ExPORTER and update missing total cost entries ----
 
 appid_cost = read_rds(here::here("data/appid-totalcost-reporterapi.rds"))
-appid_cost %<>% rename(total_cost_api = total_cost)
 
 k = nrow(exporter)
 exporter = exporter %>% left_join(appid_cost, by = "application_id") 
@@ -56,8 +55,7 @@ assertthat::assert_that(
   exporter %>% 
     filter(!is.na(total_cost), is.na(total_cost_updated)) %>% 
     nrow() == 0,
-  msg = "If cost is not missing originally, then it should not be missing after updating"
-)
+  msg = "If cost is not missing originally, then it should not be missing after updating")
 
 updated_appids = appid_cost %>% filter(!is.na(total_cost_api)) %>% pull(application_id)
 
@@ -65,10 +63,9 @@ assertthat::assert_that(
   exporter %>% 
     filter(application_id %in% updated_appids, is.na(total_cost_api)) %>% 
     nrow() == 0, 
-  msg = "Total cost missing for application ID that had non-missing total cost in RePORTER"
-)
+  msg = "Total cost missing for application ID that had non-missing total cost in RePORTER")
 
-exporter %<>% select(-total_cost_api) 
+exporter %<>% select(-c(total_cost_api, total_cost)) %>% rename(total_cost = total_cost_updated)
 
 fst::write_fst(exporter, "/Volumes/research_data/nihexporter/projects/nih_exporter_projects_apiupdate.fst")
 fst::write_fst(exporter, here::here("data/nih_exporter_projects_apiupdate.fst"))
