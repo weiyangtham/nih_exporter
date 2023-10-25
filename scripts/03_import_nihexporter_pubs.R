ad6e7c15bab1ca0cd3e8a2e64da81406990473ba
Wei Yang Tham <weiyang.tham@gmail.com>
2023-10-25 16:52:34 -0400

    incorporate file name changes

--- scripts/03_import_nihexporter_pubs.R
+++ scripts/03_import_nihexporter_pubs.R
@@ -1,9 +1,7 @@
 # Download publications
 
 
-download_pubs = function(year, destdir, name_stem = "RePORTER_PUB_C_"){
-  
-  # https://exporter.nih.gov/CSVs/final/RePORTER_PUB_C_2013.zip
+download_pubs = function(year, destdir, name_stem = "RePORTER_PUB_C_FY"){
   
   for (i in year){
     
@@ -23,41 +21,41 @@ download_pubs = function(year, destdir, name_stem = "RePORTER_PUB_C_"){
 }
 
 # destdir =  "/Volumes/Peach/nih_exporter/projects/"
-destdir = "downloaded_data/"
-purrr::map(1980:2018, ~ download_pubs(., destdir = destdir))
+destdir = "downloaded_data/pubs/"
+purrr::map(1980:2022, ~ download_pubs(., destdir = destdir))
 
 # Supplementary files containing affiliations
-destdir = "D:/nih_exporter/pubs/affils_supp"
-
-download_pubaffil = function(year, destdir, name_stem = "RePORTER_AFFLNK_C_"){
-  
-  for (i in year){
-    
-    file_url = paste0("https://exporter.nih.gov/CSVs/final/RePORTER_AFFLNK_C_", 
-                      i, ".zip")
-    
-    message(paste("Download from", file_url," \n to", 
-                  paste0(destdir, "/", name_stem, i, ".zip")))
-    
-    
-    download.file(url = file_url,
-                  dest = paste0(destdir, "/", name_stem, i, ".zip"),
-                  mode = "wb")
-    
-  }
-}
-
-purrr::map(2014:2017, ~ download_pubaffil(., destdir = destdir))
+# destdir = "D:/nih_exporter/pubs/affils_supp"
+# 
+# download_pubaffil = function(year, destdir, name_stem = "RePORTER_AFFLNK_C_"){
+#   
+#   for (i in year){
+#     
+#     file_url = paste0("https://exporter.nih.gov/CSVs/final/RePORTER_AFFLNK_C_", 
+#                       i, ".zip")
+#     
+#     message(paste("Download from", file_url," \n to", 
+#                   paste0(destdir, "/", name_stem, i, ".zip")))
+#     
+#     
+#     download.file(url = file_url,
+#                   dest = paste0(destdir, "/", name_stem, i, ".zip"),
+#                   mode = "wb")
+#     
+#   }
+# }
+# 
+# purrr::map(2014:2017, ~ download_pubaffil(., destdir = destdir))
 
 # Project-Pub crosswalks
 
 destdir = "downloaded_data/pub_link"
 
-download_publink = function(year, destdir, name_stem = "RePORTER_PUBLNK_C_"){
+download_publink = function(year, destdir, name_stem = "RePORTER_PUBLNK_C_FY"){
   
   for (i in year){
     
-    file_url = paste0("https://exporter.nih.gov/CSVs/final/RePORTER_PUBLNK_C_", 
+    file_url = paste0("https://exporter.nih.gov/CSVs/final/RePORTER_PUBLNK_C_FY", 
                       i, ".zip")
     
     message(paste("Download from", file_url," \n to", 
@@ -72,7 +70,7 @@ download_publink = function(year, destdir, name_stem = "RePORTER_PUBLNK_C_"){
   
 }
 
-purrr::map(1980:2018, ~ download_publink(., destdir = destdir))
+purrr::map(1980:2022, ~ download_publink(., destdir = destdir))
 
 
 
