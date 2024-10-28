a2dc9a6ac076d3d2d40139377d52e920eca10a46
Wei Yang Tham <weiyangtham@Weis-MacBook-Pro.local>
2024-10-28 17:20:06 -0400

    commenting edits

--- scripts/read_exporter_proj.R
+++ scripts/read_exporter_proj.R
@@ -1,30 +1,54 @@
+# 
+# # Read in zipped ExPorter project files
+# read_exporter_proj <- function(fyr, projfile = "RePORTER_PRJ_C_FY", destdir = "/Volumes/research_data/nihexporter/projects/"){
+#   
+#   proj_filename <- str_c(projfile, fyr, ".csv")
+#   # if(fyr %in% 2016:2019){
+#   #   proj_filename <- str_c(projfile, fyr, "_new", ".csv")
+#   # }
+#   
+#   message("extract ", str_c(projfile, fyr, ".csv"), " from \n", str_c(destdir, proj_filename))
+#   
+#   # Direct/indirect costs appear from 2012 onwards
+#   if (fyr < 2006){
+#     df <- readr::read_csv(unz(str_c(destdir, projfile, fyr, ".zip"), proj_filename),
+#                     col_types = cols(.default = 'c', APPLICATION_ID = 'i',
+#                                      FY = 'i', APPLICATION_TYPE = 'i',
+#                                      TOTAL_COST = 'i', TOTAL_COST_SUB_PROJECT = 'i'),
+#                     n_max = Inf)
+#   } else {
+#     df <- readr::read_csv(unz(str_c(destdir, projfile, fyr, ".zip"), proj_filename),
+#                     col_types = cols(.default = 'c', APPLICATION_ID = 'i',
+#                                      FY = 'i', APPLICATION_TYPE = 'i',
+#                                      TOTAL_COST = 'i', TOTAL_COST_SUB_PROJECT = 'i',
+#                                      DIRECT_COST_AMT = 'i', INDIRECT_COST_AMT = 'i'),
+#                     n_max = Inf)
+#   }
+#   
+#   return(list(data = df %>% rename_all(str_to_lower), 
+#          problems = problems(df) %>% mutate(fy = fyr)))
+# }
+
 
 # Read in zipped ExPorter project files
 read_exporter_proj <- function(fyr, projfile = "RePORTER_PRJ_C_FY", destdir = "/Volumes/research_data/nihexporter/projects/"){
   
-  proj_filename <- str_c(projfile, fyr, ".csv")
-  if(fyr %in% 2016:2019){
-    proj_filename <- str_c(projfile, fyr, "_new", ".csv")
-  }
-  
-  message("extract ", str_c(projfile, fyr, ".csv"), " from \n", str_c(destdir, proj_filename))
-  
-  # Direct/indirect costs appear from 2012 onwards
-  if (fyr < 2012){
-    df <- readr::read_csv(unz(str_c(destdir, projfile, fyr, ".zip"), proj_filename),
-                    col_types = cols(.default = 'c', APPLICATION_ID = 'i',
-                                     FY = 'i', APPLICATION_TYPE = 'i',
-                                     TOTAL_COST = 'i', TOTAL_COST_SUB_PROJECT = 'i'),
-                    n_max = Inf)
+    # Direct/indirect costs appear from 2006 onwards
+  if (fyr < 2006){
+    df <- readr::read_csv(paste0(destdir, projfile, fyr, ".zip"),
+                          col_types = cols(.default = 'c', APPLICATION_ID = 'i',
+                                           FY = 'i', APPLICATION_TYPE = 'i',
+                                           TOTAL_COST = 'i', TOTAL_COST_SUB_PROJECT = 'i'),
+                          n_max = Inf)
   } else {
-    df <- readr::read_csv(unz(str_c(destdir, projfile, fyr, ".zip"), proj_filename),
-                    col_types = cols(.default = 'c', APPLICATION_ID = 'i',
-                                     FY = 'i', APPLICATION_TYPE = 'i',
-                                     TOTAL_COST = 'i', TOTAL_COST_SUB_PROJECT = 'i',
-                                     DIRECT_COST_AMT = 'i', INDIRECT_COST_AMT = 'i'),
-                    n_max = Inf)
+    df <- readr::read_csv(paste0(destdir, projfile, fyr, ".zip"),
+                          col_types = cols(.default = 'c', APPLICATION_ID = 'i',
+                                           FY = 'i', APPLICATION_TYPE = 'i',
+                                           TOTAL_COST = 'i', TOTAL_COST_SUB_PROJECT = 'i',
+                                           DIRECT_COST_AMT = 'i', INDIRECT_COST_AMT = 'i'),
+                          n_max = Inf)
   }
   
   return(list(data = df %>% rename_all(str_to_lower), 
-         problems = problems(df) %>% mutate(fy = fyr)))
+              problems = problems(df) %>% mutate(fy = fyr)))
 }
