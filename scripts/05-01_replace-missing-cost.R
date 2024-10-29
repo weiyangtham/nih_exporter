90093d7d8dd635c49f70d39b81c95ad3c9f0ad3c
Wei Yang Tham <weiyangtham@Weis-MacBook-Pro.local>
2024-10-29 15:00:38 -0400

    update file names for 2023 batch

--- scripts/05-01_replace-missing-cost.R
+++ scripts/05-01_replace-missing-cost.R
@@ -9,7 +9,7 @@ library(tictoc)
 source("scripts/00_functions.R")
 
 # Find all core projects with missing total cost ----
-exporter <- fst::read_fst(here::here("data/nih_exporter_projects.fst")) %>% 
+exporter <- fst::read_fst(here::here("data/nih_exporter_projects_1985-2023.fst")) %>% 
   as_tibble()
 
 mainproj_missingcost <- exporter %>% filter(is.na(subproject_id), is.na(total_cost)) 
@@ -25,24 +25,29 @@ v = seq_last(0, n, by = step)
 # Ping API in batches of size `step` ----
 message("Pinging API takes a while. Uncomment if want to do it")
 
-# appids_list = map(head(seq_along(v), -1),
-#                   ~{a = v[.] + 1
-#                   b = v[. + 1]
-#                   k = seq(a, b, by = 1)
-#                   appids_subset <- appids[k]
-#                   ask_reporter(appids_subset)
-#                   })
-# 
-# assertthat::assert_that(length(appids_list) == (length(v) - 1),
-#                         msg = "Number of batches of records retrieved is correct")
-# appid_cost = map_df(appids_list, ~extract_reporter_variable(., "award_amount"))
-# appid_cost %<>% rename(total_cost_api = name)
-# 
-# write_rds(appid_cost, here::here("data/appid-totalcost-reporterapi.rds"))
+appids_list = map(head(seq_along(v), -1),
+                  ~{a = v[.] + 1
+                  b = v[. + 1]
+                  
+                  # range of appids being called
+                  print(sprintf("batch range: %d to %d", a, b))
+                  
+                  
+                  k = seq(a, b, by = 1)
+                  appids_subset <- appids[k]
+                  ask_reporter(appids_subset)
+                  })
+
+assertthat::assert_that(length(appids_list) == (length(v) - 1),
+                        msg = "Number of batches of records retrieved is correct")
+appid_cost = map_df(appids_list, ~extract_reporter_variable(., "award_amount"))
+appid_cost %<>% rename(total_cost_api = name)
+
+write_rds(appid_cost, here::here("data/appid-totalcost-reporterapi_1985-2013.rds"))
 
 # Link application IDs back to ExPORTER and update missing total cost entries ----
 
-appid_cost = read_rds(here::here("data/appid-totalcost-reporterapi.rds"))
+appid_cost = read_rds(here::here("data/appid-totalcost-reporterapi_1985-2013.rds"))
 
 k = nrow(exporter)
 exporter = exporter %>% left_join(appid_cost, by = "application_id") 
@@ -65,7 +70,10 @@ assertthat::assert_that(
     nrow() == 0, 
   msg = "Total cost missing for application ID that had non-missing total cost in RePORTER")
 
-exporter %<>% select(-c(total_cost_api, total_cost)) %>% rename(total_cost = total_cost_updated)
+exporter %<>% 
+  select(-c(total_cost_api, total_cost)) %>% 
+  rename(total_cost = total_cost_updated)
 
-fst::write_fst(exporter, "/Volumes/research_data/nihexporter/projects/nih_exporter_projects_apiupdate.fst")
-fst::write_fst(exporter, here::here("data/nih_exporter_projects_apiupdate.fst"))
+# fst::write_fst(exporter, "data/nih_exporter_projects_apiupdate_1985-2023.fst")
+fst::write_fst(exporter, 
+               here::here("data/nih_exporter_projects_apiupdate_1985-2023.fst"))
